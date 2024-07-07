80821bc214dffd9c596ccadaf9391a49040f5268
Javier Tischina <javier.tischina@gmail.com>
2024-07-07 01:31:08 -0600

    se agrego cambio de idioma a pipes, pipes numericos, y diseño de uncommon page

--- src/app/products/pages/numbersPage/numbersPage.component.ts
+++ src/app/products/pages/numbersPage/numbersPage.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './numbersPage.component.html',
   styleUrl: './numbersPage.component.css'
 })
-export class NumbersPageComponent { }
+export class NumbersPageComponent {
+
+  public totalSells: number = 2567789.5567;
+  public percent: number = 0.4856;
+
+}
+
